13b5a0f6f6b4672d355925c0f5fba792d57e6e14
Sanika Desai <sanikadesai17@gmail.com>
2024-07-22 13:40:52 +0530

    1

--- backend/app.py
+++ backend/app.py
@@ -184,4 +184,4 @@ def summarize():
         return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
